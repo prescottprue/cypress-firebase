21a05a40366d3d76f5a8a67dadce25eb31e96ba4
Scott Prue <sprue.dev@gmail.com>
2020-06-29 13:56:55 -0400

    Cleanup lint in lint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  'extends': [
+  extends: [
     'airbnb-base',
     'prettier',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    "plugin:jsdoc/recommended"
+    'plugin:jsdoc/recommended',
   ],
   root: true,
-  plugins: ['@typescript-eslint', 'prettier' , 'jsdoc'],
+  plugins: ['@typescript-eslint', 'prettier', 'jsdoc'],
   settings: {
     'import/resolver': {
       node: {
         moduleDirectory: ['node_modules', '/'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
     react: {
-      version: '16.0'
-    }
+      version: '16.0',
+    },
   },
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 0,
@@ -41,23 +41,23 @@ module.exports = {
       {
         singleQuote: true, // airbnb
         trailingComma: 'all', // airbnb
-      }
-    ]
+      },
+    ],
   },
   overrides: [
     {
       files: ['cmds/**'],
       rules: {
-        'comma-dangle': ['error', { 'functions': 'never' }],
+        'comma-dangle': ['error', { functions: 'never' }],
         '@typescript-eslint/explicit-function-return-type': 0,
         'prettier/prettier': [
           'error',
           {
             singleQuote: true, // airbnb
             trailingComma: 'none', // airbnb
-          }
-        ]
-      }
-    }
-  ]
-}
+          },
+        ],
+      },
+    },
+  ],
+};
