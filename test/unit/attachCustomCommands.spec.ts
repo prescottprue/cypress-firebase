a53c1d87ae25731da0befb3e3be856d99edde044
Scott Prue <sprue.dev@gmail.com>
2021-05-02 16:04:29 -0400

    fix: expand tests covering attachCustomCommands

--- test/unit/attachCustomCommands.spec.ts
+++ test/unit/attachCustomCommands.spec.ts
@@ -5,6 +5,7 @@ import { attachCustomCommands } from '../../src';
 let taskSpy: any;
 const cy: any = { log: sinon.spy() };
 const loadedCustomCommands: any = {};
+const testUserId = 'TEST_USER';
 let addSpy: sinon.SinonSpy;
 let envSpy: sinon.SinonSpy;
 let Cypress: any = {};
@@ -18,12 +19,15 @@ const firebase = {
     signInWithCustomToken,
     signOut: sinon.spy(() => Promise.resolve()),
   })),
+  database: { ServerValue: { TIMESTAMP: 'TIMESTAMP' } },
+  firestore: { Timestamp: { now: () => 'TIMESTAMP' } },
 };
 
 describe('attachCustomCommands', () => {
   beforeEach(() => {
+    currentUser = {};
     taskSpy = sinon.spy(() => Promise.resolve());
-    envSpy = sinon.spy();
+    envSpy = sinon.spy((param) => param === 'TEST_UID' && testUserId);
     onAuthStateChanged = sinon.spy((authHandleFunc) => {
       authHandleFunc({});
     });
@@ -42,6 +46,9 @@ describe('attachCustomCommands', () => {
     });
 
     it('throws if no uid is passed or within environment', () => {
+      Cypress = { Commands: { add: addSpy }, env: sinon.spy() };
+      attachCustomCommands({ cy, Cypress, firebase });
+      currentUser = undefined;
       try {
         loadedCustomCommands.login();
       } catch (err) {
@@ -52,7 +59,16 @@ describe('attachCustomCommands', () => {
       }
     });
 
-    it('calls task', async () => {
+    it('returns undefined if pass UID matches already logged in user', async () => {
+      const uid = '123ABC';
+      currentUser = { uid };
+      const returnVal = await loadedCustomCommands.login(uid);
+      expect(taskSpy).to.not.have.been.called;
+      expect(signInWithCustomToken).to.not.have.been.called;
+      expect(returnVal).to.be.undefined;
+    });
+
+    it('calls task with uid and custom claims', async () => {
       await loadedCustomCommands.login('123ABC');
       expect(taskSpy).to.have.been.calledOnce;
       expect(signInWithCustomToken).to.have.been.calledOnce;
@@ -79,7 +95,7 @@ describe('attachCustomCommands', () => {
       expect(addSpy).to.have.been.calledWith('callFirestore');
     });
 
-    it('calls task', async () => {
+    it('calls task with get action (setting third argument as options)', async () => {
       const action = 'get';
       const actionPath = '123ABC';
       await loadedCustomCommands.callFirestore(action, actionPath);
@@ -89,6 +105,43 @@ describe('attachCustomCommands', () => {
         options: undefined,
       });
     });
+    it('calls task with set action (setting third argument as data, and forth as options)', async () => {
+      const action = 'set';
+      const actionPath = '123ABC';
+      const testData = { asdf: 'asdf' };
+      const options = { withMeta: false };
+      await loadedCustomCommands.callFirestore(
+        action,
+        actionPath,
+        testData,
+        options,
+      );
+      expect(taskSpy).to.have.been.calledWith('callFirestore', {
+        action,
+        path: actionPath,
+        data: testData,
+        options,
+      });
+    });
+
+    it('calls task with set action including meta data if withMeta is set to true', async () => {
+      const action = 'set';
+      const actionPath = '123ABC';
+      const testData = { asdf: 'asdf' };
+      const options = { withMeta: true };
+      await loadedCustomCommands.callFirestore(
+        action,
+        actionPath,
+        testData,
+        options,
+      );
+      expect(taskSpy).to.have.been.calledWith('callFirestore', {
+        action,
+        path: actionPath,
+        data: { ...testData, createdBy: testUserId, createdAt: 'TIMESTAMP' },
+        options,
+      });
+    });
   });
 
   describe('cy.callRtdb', () => {
@@ -96,7 +149,7 @@ describe('attachCustomCommands', () => {
       expect(addSpy).to.have.been.calledWith('callRtdb');
     });
 
-    it('calls task', async () => {
+    it('calls task with get action (setting third argument as options)', async () => {
       const action = 'get';
       const actionPath = '123ABC';
       await loadedCustomCommands.callRtdb(action, actionPath);
@@ -106,40 +159,97 @@ describe('attachCustomCommands', () => {
         options: undefined,
       });
     });
+
+    it('calls task with set action with object (setting third argument as data, and forth as options)', async () => {
+      const action = 'set';
+      const actionPath = '123ABC';
+      const testData = { asdf: 'asdf' };
+      const options = { withMeta: false };
+      await loadedCustomCommands.callRtdb(
+        action,
+        actionPath,
+        testData,
+        options,
+      );
+      expect(taskSpy).to.have.been.calledWith('callRtdb', {
+        action,
+        path: actionPath,
+        data: testData,
+        options,
+      });
+    });
+
+    it('calls task with set action including metadata if data is object and withMeta is set to true', async () => {
+      const action = 'set';
+      const actionPath = '123ABC';
+      const testData = { asdf: 'asdf' };
+      const options = { withMeta: true };
+      await loadedCustomCommands.callRtdb(
+        action,
+        actionPath,
+        testData,
+        options,
+      );
+      expect(taskSpy).to.have.been.calledWith('callRtdb', {
+        action,
+        path: actionPath,
+        data: { ...testData, createdBy: testUserId, createdAt: 'TIMESTAMP' },
+        options,
+      });
+    });
+
+    it('calls task with set action not including metadata if data is an array and withMeta is set to true', async () => {
+      const action = 'set';
+      const actionPath = '123ABC';
+      const testData = [{ asdf: 'asdf' }];
+      const options = { withMeta: true };
+      await loadedCustomCommands.callRtdb(
+        action,
+        actionPath,
+        testData,
+        options,
+      );
+      expect(taskSpy).to.have.been.calledWith('callRtdb', {
+        action,
+        path: actionPath,
+        data: testData,
+        options,
+      });
+    });
+
+    it('calls task with set action not including metadata if data is a Date and withMeta is set to true', async () => {
+      const action = 'set';
+      const actionPath = '123ABC';
+      const testData = new Date();
+      const options = { withMeta: true };
+      await loadedCustomCommands.callRtdb(
+        action,
+        actionPath,
+        testData,
+        options,
+      );
+      expect(taskSpy).to.have.been.calledWith('callRtdb', {
+        action,
+        path: actionPath,
+        data: testData,
+        options,
+      });
+    });
   });
 
-  beforeEach(() => {
-    taskSpy = sinon.spy(() => Promise.resolve());
-    envSpy = sinon.spy();
-    onAuthStateChanged = sinon.spy((authHandleFunc) => {
-      authHandleFunc({});
+  describe('cy.getAuthUser', () => {
+    it('Is attached as a custom command', () => {
+      expect(addSpy).to.have.been.calledWith('getAuthUser');
     });
-    signInWithCustomToken = sinon.spy(() => Promise.resolve());
-    cy.task = taskSpy;
-    addSpy = sinon.spy((customCommandName: string, customCommandFunc: any) => {
-      loadedCustomCommands[customCommandName] = customCommandFunc;
+
+    it('calls task with uid', async () => {
+      const uid = 'TESTING_USER_UID';
+      await loadedCustomCommands.getAuthUser(uid);
+      expect(taskSpy).to.have.been.calledWith('getAuthUser', uid);
     });
-    Cypress = { Commands: { add: addSpy }, env: envSpy };
-    attachCustomCommands({ cy, Cypress, firebase });
   });
 
   describe('options', () => {
-    beforeEach(() => {
-      taskSpy = sinon.spy(() => Promise.resolve());
-      envSpy = sinon.spy();
-      onAuthStateChanged = sinon.spy((authHandleFunc) => {
-        authHandleFunc({});
-      });
-      signInWithCustomToken = sinon.spy(() => Promise.resolve());
-      cy.task = taskSpy;
-      addSpy = sinon.spy(
-        (customCommandName: string, customCommandFunc: any) => {
-          loadedCustomCommands[customCommandName] = customCommandFunc;
-        },
-      );
-      Cypress = { Commands: { add: addSpy }, env: envSpy };
-    });
-
     it('Aliases login command', () => {
       const commandNames = { login: 'testing' };
       attachCustomCommands({ cy, Cypress, firebase }, { commandNames });
