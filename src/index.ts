2dbecfe5531eae8d71c958c8666bbf96273cd7a0
Scott Prue <sprue.dev@gmail.com>
2021-12-01 21:49:10 -0700

    fix: bump up size limit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import attachCustomCommands from './attachCustomCommands';
 import plugin from './plugin';
 
-export { attachCustomCommands, plugin }; // eslint-disable-line import/prefer-default-export
+export { attachCustomCommands, plugin };
